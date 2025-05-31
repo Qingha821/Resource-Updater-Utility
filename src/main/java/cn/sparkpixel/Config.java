29187eeaa42d390dffe041d22a94851a4f8c77ae
Qing Huian <qingha821@outlook.com>
2025-05-31 10:24:54 +0800

    Change config to adapt the mod for Sparkpixel server

--- src/main/java/cn/sparkpixel/Config.java
+++ src/main/java/cn/sparkpixel/Config.java
@@ -21,7 +21,7 @@ import java.util.function.Supplier;
 public class Config {
 
     public final ConfigItem<String> remoteConfigUrl = new ConfigItem<>(
-            "remoteConfigUrl", JsonElement::getAsString, JsonPrimitive::new, "http://127.0.0.1/client_config.json");
+            "remoteConfigUrl", JsonElement::getAsString, JsonPrimitive::new, "http://s.bu7.top:20002/client_config.json");
 
     public final ConfigItem<List<SourceProperty>> sourceList = new ConfigItem<>(
         "sources",
