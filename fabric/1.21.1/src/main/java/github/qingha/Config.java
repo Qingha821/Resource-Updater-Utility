031541153a1d379594f21dc60084c3b2756ac349
Qingha <qingha821@outlook.com>
2026-03-03 15:27:54 +0800

    Fix everything TODO: 1.16.5 OpenGL Rendering Error

--- fabric/1.21.1/src/main/java/github/qingha/Config.java
+++ fabric/1.21.1/src/main/java/github/qingha/Config.java
@@ -21,7 +21,7 @@ import java.util.function.Supplier;
 public class Config {
 
     public final ConfigItem<String> remoteConfigUrl = new ConfigItem<>(
-            "remoteConfigUrl", JsonElement::getAsString, JsonPrimitive::new, "yoururlserver/client_config.json");
+            "remoteConfigUrl", JsonElement::getAsString, JsonPrimitive::new, "https://modelscope.cn/studio/cdpyx1/sparkpixel/resolve/master/client_config.json");
 
     public final ConfigItem<List<SourceProperty>> sourceList = new ConfigItem<>(
         "sources",
